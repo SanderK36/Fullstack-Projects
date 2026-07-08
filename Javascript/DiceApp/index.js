66b1172c58cb92b1d2be16f513449d1d5f2dec37
SanderK36 <sander.k606@gmail.com>
2026-07-08 21:02:58 +0200

    08.07

---
+++ Javascript/DiceApp/index.js
@@ -0,0 +1,6 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+    alert("i got clicked")
+})
+}

bbe701edbba7505144028b4e01627d7be4d3deea
SanderK36 <sander.k606@gmail.com>
2026-07-17 21:37:31 +0200

    17.07.26

--- jQuery/first project/index.js
+++ jQuery/first project/index.js
@@ -1,3 +1,15 @@
-$("document").ready(function () {
-    $("h1").css("color", "red");
+
+
+$("button").click(function() {
+    $("h1").css("color", "purple")
+})
+
+$("a").attr("href", "https://www.yahoo.com")
+
+$("body").keypress(function(event) {
+    $("h1").text(event.key)
+})
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red")
 })
